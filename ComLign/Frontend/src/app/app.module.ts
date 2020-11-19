be9590d48c581dbb38ad901f50f226229413414d
BryanJCuellar <bjcz1998@gmail.com>
2020-11-19 03:01:11 -0600

    Frontend Update - Form Registro

--- ComLign/Frontend/src/app/app.module.ts
+++ ComLign/Frontend/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { LayoutModule } from '@angular/cdk/layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeComponent } from './pages/home/home.component';
 import { CompaniesComponent } from './pages/companies/companies.component';
 import { SigninComponent } from './pages/signin/signin.component';
@@ -13,18 +14,19 @@ import { BusinessComponent } from './pages/login/business/business.component';
 import { AdminComponent } from './pages/login/admin/admin.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HelpComponent } from './pages/help/help.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PlansComponent } from './pages/plans/plans.component';
+import { PlanPaymentComponent } from './pages/plan-payment/plan-payment.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { BusinessHomeComponent } from './pages/business-home/business-home.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { MuestraEmpresasComponent } from './shared/components/muestra-empresas/muestra-empresas.component';
 import { ListaEmpresasComponent } from './shared/components/lista-empresas/lista-empresas.component';
 import { CommonLogoComponent } from './shared/components/common-logo/common-logo.component';
-import { PlansComponent } from './pages/plans/plans.component';
-import { PlanPaymentComponent } from './pages/plan-payment/plan-payment.component';
 import { CommonLogoDarkComponent } from './shared/components/common-logo-dark/common-logo-dark.component';
-import { BusinessHomeComponent } from './pages/business-home/business-home.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -55,6 +57,8 @@ import { BusinessHomeComponent } from './pages/business-home/business-home.compo
     NgbModule,
     FontAwesomeModule,
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [
     Title
--- ComLign/Frontend/src/app/pages/signin/signin.component.ts
+++ ComLign/Frontend/src/app/pages/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +8,103 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-  tipoUsuarioSeleccionado:any;
+  tipoUsuarioSeleccionado: any;
+
+  formularioAuxiliar = new FormGroup({
+    tipoUsuario: new FormControl('', [Validators.required])
+  });
+
+  formularioRegistro = new FormGroup({
+    tipoUsuario: new FormControl('', [Validators.required]),
+    nombre: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    apellido: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    nombreEmpresa: new FormControl(''),
+    descripcionEmpresa: new FormControl(''),
+    codigoAdministrador: new FormControl('')
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  //Metodos get
+  get tipoUsuario() {
+    return this.formularioAuxiliar.get('tipoUsuario');
+  }
+  get nombre() {
+    return this.formularioRegistro.get('nombre');
+  }
+  get apellido() {
+    return this.formularioRegistro.get('apellido');
+  }
+  get email() {
+    return this.formularioRegistro.get('email');
+  }
+  get password() {
+    return this.formularioRegistro.get('password');
+  }
+  get nombreEmpresa() {
+    return this.formularioRegistro.get('nombreEmpresa');
+  }
+  get descripcionEmpresa() {
+    return this.formularioRegistro.get('descripcionEmpresa');
+  }
+  get codigoAdministrador() {
+    return this.formularioRegistro.get('codigoAdministrador');
+  }
+
+  cambiarTipoUsuario(tipo) {
+    this.tipoUsuarioSeleccionado = tipo;
+    //Cliente
+    if (tipo == "Cliente") {
+      this.formularioRegistro = new FormGroup({
+        tipoUsuario: new FormControl(tipo, [Validators.required]),
+        nombre: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+        apellido: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+        email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+        password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+        nombreEmpresa: new FormControl(''),
+        descripcionEmpresa: new FormControl(''),
+        codigoAdministrador: new FormControl('')
+      });
+    }
+    //Empresa
+    if (tipo == "Empresa") {
+      this.formularioRegistro = new FormGroup({
+        tipoUsuario: new FormControl(tipo, [Validators.required]),
+        nombre: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+        apellido: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+        email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+        password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+        nombreEmpresa: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+        descripcionEmpresa: new FormControl('', [Validators.required, Validators.maxLength(500)]),
+        codigoAdministrador: new FormControl('')
+      });
+    }
+    //Admin
+    if (tipo == "Administrador") {
+      this.formularioRegistro = new FormGroup({
+        tipoUsuario: new FormControl(tipo, [Validators.required]),
+        nombre: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+        apellido: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+        email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+        password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+        nombreEmpresa: new FormControl(''),
+        descripcionEmpresa: new FormControl(''),
+        codigoAdministrador: new FormControl('', [Validators.required])
+      });
+    }
+    //console.log(tipo);
+    //console.log(this.formularioRegistro.value);
+  }
+
+  cambiarInput(input: any): any {
+    input.type = input.type === 'password' ? 'text' : 'password';
+  }
+
+
 
 }
